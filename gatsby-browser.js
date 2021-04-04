9dd226e2f4d3701e8046fe801c0622e96b84bd8a
mnogiec <nogiecmikolaj@gmail.com>
2021-04-04 20:16:18 +0200

    Wrap root in ContextProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import React from 'react';
+import { Provider as ContextProvider } from 'context/ThemeContext';
 
-// You can delete this file if you're not using it
+
+export const wrapRootElement = ({ element }) => (
+    <ContextProvider>
+      {element}
+    </ContextProvider>
+  );
