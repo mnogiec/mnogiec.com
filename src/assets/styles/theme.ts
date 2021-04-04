76722dde24fcaf2bcd7ed80c7cfd7555f7578110
mnogiec <nogiecmikolaj@gmail.com>
2021-04-04 17:54:55 +0200

    Create a complete theme for colors and sizes (both themes)

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,57 @@
+export default {
+  color: {
+    primary: {
+      light: '#3A3D9B',
+      dark: '#D2B16A',
+    },
+    background: {
+      light: '#FFFFFF',
+      dark: '#101010',
+    },
+    sectionGradient: {
+      start: {
+        light: '#F8F8FB',
+        dark: 'transparent',
+      },
+      end: {
+        light: '#FFFFFF',
+        dark: 'transparent',
+      },
+    },
+    headerBackground: {
+      light: '#FFFFFF',
+      dark: '#1F1F1F',
+    },
+    font: {
+      light: '#111111',
+      dark: '#FFFFFF',
+    },
+    subHeading: {
+      light: '#665F82',
+      dark: '#AEA48F',
+    },
+    welcomeBalls: {
+      light: ['#3A9B91', '#3A3D9B', '#959B3A', '#9B3A3A', '#953A9B'],
+      dark: ['#3A9B91', '#3A3D9B', '#959B3A', '#9B3A3A', '#953A9B'],
+    },
+  },
+  font: {
+    family: {
+      lato: "'Montserrat', sans-serif",
+    },
+    size: {
+      xs: '1rem',
+      s: '1.6rem',
+      m: '2.0rem',
+      l: '2.4rem',
+      xl: '3.6rem',
+      xxl: '8.5rem',
+    },
+    weight: {
+      light: '300',
+      normal: '400',
+      bold: '700',
+    },
+  },
+  shadow: '0.2rem 0.4rem 1.5rem rgba(25,25,25,0.15)',
+};
