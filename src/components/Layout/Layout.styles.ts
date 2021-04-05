c3c7ce7338ba28d19d07d5f6d384919802f5f41b
mnogiec <nogiecmikolaj@gmail.com>
2021-04-05 17:30:21 +0200

    Remove redundant import

--- src/components/Layout/Layout.styles.ts
+++ src/components/Layout/Layout.styles.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 
-import media from 'assets/styles/media';
 import theme from 'assets/styles/theme';
 
 export const StyledWrapper = styled.div<{header?: boolean, sideMenu?: boolean, showSideMenu?: boolean}>`
