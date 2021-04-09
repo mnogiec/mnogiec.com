996692352278999c9962d2a739f68fc0fc2d6421
mnogiec <nogiecmikolaj@gmail.com>
2021-04-09 18:13:03 +0200

    Create hero section with cookies notif. and header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,58 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
+import { useIntl } from 'gatsby-plugin-intl';
+
+import { Context as GlobalContext } from 'context/GlobalContext';
+import Layout from 'components/Layout/Layout';
+import Button from 'components/Button/Button';
+import CookiesNotification from 'components/CookiesNotification/CookiesNotification';
+
+import * as S from 'pages_styles/index.styles';
+
+import computerDarkSvg from 'assets/images/hero/computer_dark.svg';
+import computerLightSvg from 'assets/images/hero/computer_light.svg';
+import githubSvg from 'assets/images/hero/github.svg';
+import linkedinSvg from 'assets/images/hero/linkedin.svg';
+import mailSvg from 'assets/images/hero/mail.svg';
 
 
-const IndexPage = () => (
-  <h1>index</h1>
-);
+const IndexPage = () => {
+  const intl = useIntl();
+  const { state } = useContext(GlobalContext);
+
+
+  return (
+    <Layout pageTitle="Strona główna" header>
+      <CookiesNotification show={!state.hadDisplayedCookies} />
+      <S.StyledHeroWrapper>
+        <S.StyledHeroMainBox>
+          <S.StyledHeroTitlesWrapper>
+            <S.StyledHeroTitle>{intl.formatMessage({ id: 'hero.title' })}</S.StyledHeroTitle>
+            <S.StyledHeroSubtitle>{intl.formatMessage({ id: 'hero.subtitle' })}</S.StyledHeroSubtitle>
+          </S.StyledHeroTitlesWrapper>
+          <S.StyledHeroButtonsWrapper>
+            <Button bold clicked={() => null}>{intl.formatMessage({ id: 'hero.projectsButton' })}</Button>
+            <Button bold basic clicked={() => null}>{intl.formatMessage({ id: 'hero.contactButton' })}</Button>
+          </S.StyledHeroButtonsWrapper>
+          <S.StyledHeroImage src={state.theme === 'light' ? computerLightSvg : computerDarkSvg} alt="" />
+        </S.StyledHeroMainBox>
+        <S.StyledHeroBottomBar>
+          <S.StyledHeroSocialLink href="https://www.linkedin.com/in/nogiecmikolaj/" target="_blank">
+            <S.StyledHeroSocialIcon alt="LinkedIn" src={linkedinSvg} />
+          </S.StyledHeroSocialLink>
+          <S.StyledHeroSocialLink href="https://github.com/mnogiec" target="_blank">
+            <S.StyledHeroSocialIcon alt="GitHub" src={githubSvg} />
+          </S.StyledHeroSocialLink>
+          <S.StyledHeroSocialLink href="mailto:nogiecmikolaj@gmail.com" target="_blank">
+            <S.StyledHeroSocialIcon alt="Mail" src={mailSvg} />
+          </S.StyledHeroSocialLink>
+        </S.StyledHeroBottomBar>
+      </S.StyledHeroWrapper>
+      <S.StyledPageWrapper>
+        strona
+      </S.StyledPageWrapper>
+    </Layout>
+  );
+};
 
 
 export default IndexPage;
---
+++ src/pages_styles/index.styles.ts
@@ -0,0 +1,181 @@
+import styled from 'styled-components';
+import media from 'assets/styles/media';
+
+
+export const StyledPageWrapper = styled.main`
+  width: 100%;
+  padding: 0 3rem;
+
+  @media ${media.laptopL}{
+    width: 125rem;
+    margin: 0 auto;
+  }
+`;
+
+export const StyledBubblesContainer = styled.div`
+  position: absolute;
+  top: ${({ theme }) => theme.const.HEADER_HEIGHT};
+  left: 0;
+  width: 100%;
+  height: ${({ theme }) => `calc(100vh - ${theme.const.HEADER_HEIGHT})`};
+  overflow: hidden;
+`;
+
+export const StyledHeroWrapper = styled.div`
+  width: 100%;
+  position: relative;
+  padding: 0 2rem;
+  padding-top: 2rem;
+  height: ${({ theme }) => `calc(100vh - ${theme.const.HEADER_HEIGHT})`};
+  display: grid;
+  grid-template-rows: 1fr min-content;
+
+  @media ${media.tablet}{
+    padding: 0 3.5rem;
+  }
+
+  @media ${media.laptop}{
+    padding: 0 5rem;
+  }
+
+  @media ${media.laptopL}{
+    padding: 0 6.5rem;
+  }
+
+  @media ${media.desktop}{
+    width: 155rem;
+    margin: 0 auto;
+  }
+`;
+
+export const StyledHeroMainBox = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: [text-start] max-content [text-end image-start] max-content [image-end buttons-start] max-content [buttons-end];
+  justify-content: space-evenly;
+  align-content: space-evenly;
+  align-items: center;
+
+  @media ${media.tabletL}{
+    grid-template-columns: [start] max-content [middle] max-content [end];
+    grid-template-rows: [start] max-content [middle] max-content [end];
+    justify-content: space-between;
+    align-content: center;
+  }
+`;
+
+export const StyledHeroTitlesWrapper = styled.div`
+  grid-row: text-start/text-end;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-size: 4rem;
+
+  @media ${media.mobileM}{
+    font-size: 4.5rem;
+  }
+
+  @media ${media.tablet}{
+    font-size: 5.5rem;
+  }
+
+  @media ${media.tabletL}{
+    align-items: initial;
+    grid-row: start/middle;
+    grid-column: start/middle;
+  }
+
+  @media ${media.laptopL}{
+    font-size: 6.5rem;
+  }
+
+  @media ${media.desktop}{
+    font-size: ${({ theme }) => theme.font.size.xxl};
+  }
+`;
+
+export const StyledHeroTitle = styled.p`
+  color: var(--color-primary);
+  text-align: center;
+
+  @media ${media.tabletL}{
+    text-align: initial;
+  }
+`;
+
+export const StyledHeroSubtitle = styled.h1`
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  text-align: center;
+
+  @media ${media.tabletL}{
+    text-align: initial;
+  }
+`;
+
+export const StyledHeroButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  grid-row: button-start/buttons-end;
+
+  @media ${media.tabletL}{
+    flex-direction: row;
+    grid-row: middle/end;
+    grid-column: start/middle;
+
+    &>button:first-child{
+      margin-right: 2rem;
+    }
+  }
+`;
+
+export const StyledHeroImage = styled.img`
+  display: block;
+  width: 75vw;
+  max-width: 50rem;
+  margin: 0 auto;
+  grid-row: image-start/image-end;
+  transform: rotate(-3deg);
+  
+  @media ${media.tabletL}{
+    max-width: 45rem;
+    margin-right: 2rem;
+    transform: initial;
+    grid-row: start/end;
+    grid-column: middle/end;
+  }
+
+  @media ${media.laptopL}{
+    max-width: 55rem;
+  }
+
+  @media ${media.desktop}{
+    width: 60rem;
+    max-width: initial;
+  }
+`;
+
+export const StyledHeroBottomBar = styled.div`
+  padding: 4rem 5rem;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  @media ${media.tabletL}{
+    padding: 4rem 0;
+    justify-content: flex-end;
+  }
+`;
+
+export const StyledHeroSocialLink = styled.a`
+  margin: 0 1rem;
+`;
+
+export const StyledHeroSocialIcon = styled.img`
+  display: block;
+  width: 3rem;
+
+  @media ${media.laptop}{
+    width: 2.5rem;
+  }
+`;
