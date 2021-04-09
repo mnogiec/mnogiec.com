f4e1d7fb500f678e02fa6394fc5644ca67f6a265
mnogiec <nogiecmikolaj@gmail.com>
2021-04-09 19:45:48 +0200

    Create technologies section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,23 @@ import linkedinSvg from 'assets/images/hero/linkedin.svg';
 import mailSvg from 'assets/images/hero/mail.svg';
 import aboutImage from 'assets/images/about/myself.jpg';
 
+import adobeXdIcon from 'assets/images/technologies/adobe_xd.png';
+import cssIcon from 'assets/images/technologies/css.png';
+import gatsbyJsIcon from 'assets/images/technologies/gatsby_js.png';
+import gitIcon from 'assets/images/technologies/git.png';
+import htmlIcon from 'assets/images/technologies/html.png';
+import javascriptIcon from 'assets/images/technologies/javascript.png';
+import mongoDBIcon from 'assets/images/technologies/mongoDB.png';
+import mySqlIcon from 'assets/images/technologies/mysql.png';
+import nodeJsIcon from 'assets/images/technologies/node_js.png';
+import pythonIcon from 'assets/images/technologies/python.png';
+import reactNativeIcon from 'assets/images/technologies/react_native.png';
+import reactIcon from 'assets/images/technologies/react.png';
+import reduxIcon from 'assets/images/technologies/redux.png';
+import sassIcon from 'assets/images/technologies/sass.png';
+import typescriptIcon from 'assets/images/technologies/typescript.png';
+
+
 const IndexPage = () => {
   const intl = useIntl();
   const { state } = useContext(GlobalContext);
@@ -64,6 +81,79 @@ const IndexPage = () => {
               {intl.formatMessage({ id: 'about.text' })}
             </Text>
           </S.StyledAbout>
+        </S.StyledSectionWrapper>
+      </S.StyledSection>
+      {/* TECHNOLOGIES */}
+      <S.StyledSection>
+        <S.StyledSectionWrapper>
+          <Heading
+            title={intl.formatMessage({ id: 'technologies.title' })}
+            subtitle={intl.formatMessage({ id: 'technologies.subtitle' })}
+          >
+            {intl.formatMessage({ id: 'technologies.title_text' })}
+          </Heading>
+          <S.StyledTechnologies>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={reactIcon} alt="React" />
+              <Text bold>React</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={typescriptIcon} alt="Typescript" />
+              <Text bold>Typescript</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={reduxIcon} alt="Redux" />
+              <Text bold>Redux</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={gatsbyJsIcon} alt="Gatsby.js" />
+              <Text bold>Gatsby.js</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={reactNativeIcon} alt="React Native" />
+              <Text bold>React Native</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={javascriptIcon} alt="Javascript" />
+              <Text bold>Javascript</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={htmlIcon} alt="HTML" />
+              <Text bold>HTML</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={cssIcon} alt="CSS" />
+              <Text bold>CSS</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={sassIcon} alt="Sass" />
+              <Text bold>Sass</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={nodeJsIcon} alt="Node.js" />
+              <Text bold>Node.js</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={mongoDBIcon} alt="MongoDB" />
+              <Text bold>MongoDB</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={mySqlIcon} alt="MySQL" />
+              <Text bold>MySQL</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={pythonIcon} alt="Python" />
+              <Text bold>Python</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={gitIcon} alt="Git" />
+              <Text bold>Git</Text>
+            </S.StyledTechnologiesBox>
+            <S.StyledTechnologiesBox>
+              <S.StyledTechnologiesImage src={adobeXdIcon} alt="Adobe Xd" />
+              <Text bold>Adobe Xd</Text>
+            </S.StyledTechnologiesBox>
+          </S.StyledTechnologies>
         </S.StyledSectionWrapper>
       </S.StyledSection>
     </Layout>
