b86030974b6c95aab0c53f15115a357087392b6b
mnogiec <nogiecmikolaj@gmail.com>
2021-04-13 21:24:37 +0200

    Create 404 page to redirect to index

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,34 @@
-import * as React from 'react';
+import { useEffect, useContext } from 'react';
+import { navigate } from 'gatsby';
+import { Context as GlobalContext } from 'context/GlobalContext';
 
 
-const NotFoundPage = () => (
-  <h1>not found</h1>
-);
+const NotFoundPage = () => {
+  const { state } = useContext(GlobalContext);
+
+
+  useEffect(():any => {
+    switch (state.language) {
+      case 'en': {
+        return navigate('/');
+      }
+
+      case 'de': {
+        return navigate('/de');
+      }
+
+      case 'pl': {
+        return navigate('/pl');
+      }
+
+      default: {
+        return navigate('/');
+      }
+    }
+  }, []);
+
+  return null;
+};
 
 
 export default NotFoundPage;
