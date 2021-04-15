4abb467798d1ac42fcc2ae986e9d33c071bd8814
mnogiec <nogiecmikolaj@gmail.com>
2021-04-15 23:24:58 +0200

    Activate transporter

--- src/server/app.js
+++ src/server/app.js
@@ -53,23 +53,23 @@ app.post('/api/mail', async (req, res) => {
   try {
     const { name, email, message } = req.body;
 
-    // await transporter.sendMail({
-    //   from: `MNogiec.com <${process.env.MAIL_USER}>`,
-    //   to: process.env.OWNER_MAIL,
-    //   subject: `[MNogiec.com] New message from ${name}`,
-    //   text: `${name} <${email}> - ${message}`,
-    //   html: `
-    //     <p style="margin: 20px 0; font-size: 1rem;">
-    //       <b>Name:</b> ${name}
-    //     </p>
-    //     <p style="margin: 20px 0;  font-size: 1rem;">
-    //       <b>E-mail address:</b> ${email}
-    //     </p>
-    //     <p style="margin: 20px 0;  font-size: 1rem;">
-    //       ${message}
-    //     </p>
-    //   `,
-    // });
+    await transporter.sendMail({
+      from: `MNogiec.com <${process.env.MAIL_USER}>`,
+      to: process.env.OWNER_MAIL,
+      subject: `[MNogiec.com] New message from ${name}`,
+      text: `${name} <${email}> - ${message}`,
+      html: `
+        <p style="margin: 20px 0; font-size: 1rem;">
+          <b>Name:</b> ${name}
+        </p>
+        <p style="margin: 20px 0;  font-size: 1rem;">
+          <b>E-mail address:</b> ${email}
+        </p>
+        <p style="margin: 20px 0;  font-size: 1rem;">
+          ${message}
+        </p>
+      `,
+    });
 
     return res.status(200).send({ code: 200, msg: 'Message sent' });
   } catch (error) {
